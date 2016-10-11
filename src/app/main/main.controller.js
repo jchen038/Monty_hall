c4243f5896b50c3b85e8362abe21451c94f8a65f
Jimmy Chen <mfwinds@gmail.com>
2016-10-11 00:02:13 -0700

    Prototype

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,10 +3,51 @@
 
   angular
     .module('threeDoor')
-    .controller('MainController', MainController);
+    .controller('MainController', ['$scope', '$timeout', MainController]);
 
   /** @ngInject */
-  function MainController($scope) {
-    $scope.totalPlays = 0;
+  function MainController($scope, $timeout) {
+    $scope.switch = false;
+    $scope.totalPlays = 1000;
+    $scope.results = {total: 0, win: 0, lose: 0};
+
+    $scope.toggleSwitch = function() {
+      if($scope.switch) {
+        $scope.switch = false;
+      } else {
+        $scope.switch = true;
+      }
+    }
+
+    $scope.letsGo = function() {
+      var doors = [1, 2, 3];
+      $scope.results = {total: 0, win: 0, lose: 0};
+
+      for(var count = 1; count <= $scope.totalPlays; count++) {
+        $timeout(function() {
+          var pick = Math.floor(Math.random() * 1000) % 3;
+          var answer = Math.floor(Math.random() * 1000) % 3
+          if($scope.switch) {
+            if(pick !== answer) {
+              $scope.results.win += 1;
+            } else {
+              $scope.results.lose += 1;
+            }
+          } else {
+            if(pick === answer) {
+              $scope.results.win += 1;
+            } else {
+              $scope.results.lose += 1;
+            }
+          }
+
+          $scope.results.total += 1;
+        }, 500);
+      }
+    }
+
+    $scope.reset = function() {
+
+    }
   }
 })();
